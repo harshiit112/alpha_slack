8068425440534d02144cae13e0846ddd0cbd51f9
harshitit112 <hv702383@gmail.com>
2025-09-20 02:48:58 +0530

    adding auth and using clerk for auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,36 @@
 import express from "express";
-import { ENV } from "./config/env.js"; 
+import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
 
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+        connectDB();
+      });
+    }
+  } catch (error) {
+    console.error("Error staring servers:", error);
+    process.exit(1);
+  }
+};
 
-app.listen(ENV.PORT, () => {
-    console.log("Server started on port:", ENV.PORT);
-    connectDB();
-});
+startServer();
+
+export default app;
