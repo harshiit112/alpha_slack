4b5a5ceadcfccbd4c6426739b2b7645210d16e1f
harshitit112 <hv702383@gmail.com>
2025-09-23 02:47:32 +0530

    auth page and homepage implemented

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+    Homepage
+    </div>
+  )
+}
+
+export default HomePage
